c6027fe8b87173e427ce098277bdb4359390f20f
williamlimasilva <williamtakuma@yahoo.com.br>
2025-03-07 14:51:29 -0300

    feat(frontend): Add main layout components and styles for blog structure

---
+++ desafios/desafio1/Criando um Blog com Angular/angular-blog/src/app/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
+import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
+import { SmallCardComponent } from '../../components/small-card/small-card.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    BigCardComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    SmallCardComponent,
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
