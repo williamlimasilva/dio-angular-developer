30bbbf93d171f3fc9d0f7d41f70e74ef74e6152e
williamlimasilva <williamtakuma@yahoo.com.br>
2025-03-12 16:58:35 -0300

    feat(frontend): Update quiz functionality and styles, replace assets, and enhance responsiveness

--- desafios/desafio3/Criando um Clone do BuzzFeed com Angular/angular-buzzfeed-quizz-clone/src/app/components/quizz/quizz.component.ts
+++ desafios/desafio3/Criando um Clone do BuzzFeed com Angular/angular-buzzfeed-quizz-clone/src/app/components/quizz/quizz.component.ts
@@ -1,9 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../../public/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
   imports: [],
   templateUrl: './quizz.component.html',
-  styleUrl: './quizz.component.css',
+  styleUrls: ['./quizz.component.css', './quizz.responsive.component.css'],
 })
-export class QuizzComponent {}
+export class QuizzComponent implements OnInit {
+  [x: string]: any;
+  title: string = 'TITLE';
+
+  questions: any;
+  questionSelected: any = 'Selected Question';
+
+  answers: string[] = [];
+  answerSelected: string = 'Selected Answer';
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  showRestartButton: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  selectOption(value: string) {
+    this.answers.push(value);
+    this.answerSelected = value;
+    this.nextStep();
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionIndex < this.questionMaxIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected =
+        quizz_questions.results[
+          finalAnswer as keyof typeof quizz_questions.results
+        ];
+
+      setTimeout(() => {
+        this.showRestartButton = true;
+      }, 1000);
+    }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = await answers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+
+    return result;
+  }
+
+  restart() {
+    this.finished = false;
+    this.showRestartButton = false;
+    this.questionIndex = 0;
+    this.questionSelected = this.questions[this.questionIndex];
+    this.answers = [];
+    this.answerSelected = '';
+  }
+}
